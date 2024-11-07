43be2a1dbfa32e66a05a22d5e46518fbb574fdc3
parari415 <parari415@gmail.com>
2024-11-07 22:18:23 +0900

    relationships

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,10 @@
 class Recipe < ApplicationRecord
 end
+class Recipe < ApplicationRecord
+    has_many :ingredients, dependent: :destroy
+    has_many :procedures, dependent: :destroy
+  
+    # 材料と手順を一緒に保存
+    accepts_nested_attributes_for :ingredients, allow_destroy: true
+    accepts_nested_attributes_for :procedures, allow_destroy: true
+  end  
